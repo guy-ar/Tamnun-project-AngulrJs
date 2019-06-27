da5d4a9adafa169cc99bc7fdf7ab85a7f2bfcfe5
guyar <guy.arbilly@gmail.com>
2019-06-27 13:20:28 +0300

    logic in navbar to show menus for admin and add new menu items for users

--- app/shared/navbar/navbarCtrl.js
+++ app/shared/navbar/navbarCtrl.js
@@ -1,9 +1,14 @@
 
 scheduleApp.controller("navbarCtrl", function($scope, userSrv, $location, $log) {
 
+    $scope.preventMenu = ["/login", "/error"];
 
     $scope.isLoggedIn = function() {
         return userSrv.isLoggedIn();
+    }
+
+    $scope.isLoggedAdmin = function() {
+        return userSrv.isLoggedAdmion();
     }
 
     $scope.logout = function() {
@@ -14,7 +19,8 @@ scheduleApp.controller("navbarCtrl", function($scope, userSrv, $location, $log) 
     // navbar will not be presetned on login/sign-up pages
     $scope.isShowMenu = function() {
         $log.info("current path:" + $location.$$path);
-        if ($location.$$path == "/login") {
+        // if ($location.$$path == "/login") {
+        if ($scope.preventMenu.indexOf($location.$$path) >=0) {
             $log.info("login - do not show menu");
             return false;
         } else {
