b40f5268a59d08463d22bbfcd22f0c2e5b24ed4b
guyar <guy.arbilly@gmail.com>
2019-07-03 12:16:48 +0300

    add date picker directive

---
+++ app/shared/calendar/tamnunDatePickerDir.js
@@ -0,0 +1,8 @@
+scheduleApp.directive("tamnunDatePicker", function() {
+
+    return {
+        templateUrl: "/app/shared/calendar/datePicker.html",
+        controller: "datePickerCtrl"
+    }
+
+})
