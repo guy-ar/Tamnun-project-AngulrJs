1d356641e3dbdbd3fcc4eb00b6adcd85c0362a84
guyar <guy.arbilly@gmail.com>
2019-07-03 11:30:00 +0300

    add logic to prevent  access the event dtls without login

--- app/components/event/eventDtlsCtrl.js
+++ app/components/event/eventDtlsCtrl.js
@@ -1,6 +1,12 @@
-scheduleApp.controller("eventDtlsCtrl", function($scope, $log, eventSrv, $routeParams) {
+scheduleApp.controller("eventDtlsCtrl", function($scope, $log, eventSrv, userSrv, $routeParams) {
   
-    // Calling the service to get the car with the index
+  // if user is not logged in - go to home
+  if (!userSrv.isLoggedIn()) {
+    $location.path("/");
+    return;
+}
+
+    // Calling the service to get the event with the key
     // Notice how we are accessing $routeParams.id to get the dynamic part in the URL
     eventSrv.getEventById($routeParams.id).then(function(event) {
       $scope.event = event;
